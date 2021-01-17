8c776a5db44752b9ab3bd3aeebdf06f91217549c
kevinzhao2233 <kevin_2233@qq.com>
2021-01-17 11:39:06 +0800

    📦 添加校验

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,12 +1,15 @@
 /**
+ * 是否为外链
  * @param {string} path
  * @returns {Boolean}
  */
 export function isExternal(path) {
-  return /^(https?:|mailto:|tel:)/.test(path)
+  const reg = /^(https?:|mailto:|tel:)/
+  return reg.test(path)
 }
 
 /**
+ * 是否为 url（只支持 http、https、ftp 协议）
  * @param {string} url
  * @returns {Boolean}
  */
@@ -16,6 +19,27 @@ export function validURL(url) {
 }
 
 /**
+ * 判断是否为IP
+ * @param ip
+ * @returns {boolean}
+ */
+export function isIP(ip) {
+  const reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+  return reg.test(ip)
+}
+
+/**
+ * 判断是否是端口号
+ * @param str
+ * @returns {boolean}
+ */
+export function isPort(str) {
+  const reg = /^([0-9]|[1-9]\d|[1-9]\d{2}|[1-9]\d{3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$/
+  return reg.test(str)
+}
+
+/**
+ * 是否为小写字母
  * @param {string} str
  * @returns {Boolean}
  */
@@ -25,6 +49,7 @@ export function validLowerCase(str) {
 }
 
 /**
+ * 是否为大写字母
  * @param {string} str
  * @returns {Boolean}
  */
@@ -34,6 +59,7 @@ export function validUpperCase(str) {
 }
 
 /**
+ * 是否为字母（无所谓大小写）
  * @param {string} str
  * @returns {Boolean}
  */
@@ -43,6 +69,17 @@ export function validAlphabets(str) {
 }
 
 /**
+ * 判断是否中文
+ * @param str
+ * @returns {boolean}
+ */
+export function isChinese(str) {
+  const reg = /^[\u4E00-\u9FA5]{2,4}$/
+  return reg.test(str)
+}
+
+/**
+ * 是否为 Email
  * @param {string} email
  * @returns {Boolean}
  */
@@ -52,6 +89,22 @@ export function validEmail(email) {
 }
 
 /**
+ * 判断是否为空
+ * @param str
+ * @returns {boolean}
+ */
+export function isBlank(str) {
+  return (
+    str == null ||
+    false ||
+    str === '' ||
+    str.trim() === '' ||
+    str.toLocaleLowerCase().trim() === 'null'
+  )
+}
+
+/**
+ * 是否为字符串
  * @param {string} str
  * @returns {Boolean}
  */
@@ -63,6 +116,17 @@ export function isString(str) {
 }
 
 /**
+ * 判断是否为数字
+ * @param {any} value
+ * @returns {boolean}
+ */
+export function isNumber(value) {
+  const reg = /^[0-9]*$/
+  return reg.test(value)
+}
+
+/**
+ * 是否为数组
  * @param {Array} arg
  * @returns {Boolean}
  */
@@ -74,7 +138,7 @@ export function isArray(arg) {
 }
 
 /**
- * 判断是否是json
+ * 是否是json
  * @param str
  * @returns {boolean}
  */
@@ -88,4 +152,86 @@ export function isJSON(str) {
     }
   }
   return false
+}
+
+/**
+ * 判断是否是名称
+ * @param value
+ * @returns {boolean}
+ */
+export function isName(value) {
+  const reg = /^[\u4e00-\u9fa5a-zA-Z0-9]+$/
+  return reg.test(value)
+}
+
+/**
+ * 校验密码是否由8-16位包含大写字母、小写字母、数字、特殊字符的任意三种组合
+ * @param str
+ * @returns {boolean}
+ */
+export function isPassword(str) {
+  const reg = /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_]+$)(?![a-z0-9]+$)(?![a-z\W_]+$)(?![0-9\W_]+$)[a-zA-Z0-9\W_]{8,16}$/
+  return reg.test(str)
+}
+
+/**
+ * 判断是否是手机号
+ * @param str
+ * @returns {boolean}
+ */
+export function isPhone(str) {
+  const reg = /^1\d{10}$/
+  return reg.test(str)
+}
+
+/**
+ * 判断是否为固话
+ * @param str
+ * @returns {boolean}
+ */
+export function isTel(str) {
+  const reg = /^(400|800)([0-9\\-]{7,10})|(([0-9]{4}|[0-9]{3})(-| )?)?([0-9]{7,8})((-| |转)*([0-9]{1,4}))?$/
+  return reg.test(str)
+}
+
+/**
+ * 判断是否是身份证号(第二代)
+ * @param str
+ * @returns {boolean}
+ */
+export function isIdCard(str) {
+  const reg = /^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
+  return reg.test(str)
+}
+
+/**
+ * 判断经度 -180.0～+180.0（整数部分为0～180，必须输入1到5位小数）
+ * @param str
+ * @returns {boolean}
+ */
+export function isLongitude(str) {
+  const reg = /^[-|+]?(0?\d{1,2}\.\d{1,5}|1[0-7]?\d{1}\.\d{1,5}|180\.0{1,5})$/
+  return reg.test(str)
+}
+
+/**
+ * 判断纬度 -90.0～+90.0（整数部分为0～90，必须输入1到5位小数）
+ * @param str
+ * @returns {boolean}
+ */
+export function isLatitude(str) {
+  const reg = /^[-|+]?([0-8]?\d{1}\.\d{1,5}|90\.0{1,5})$/
+  return reg.test(str)
+}
+
+/**
+ * rtsp校验，只要有rtsp://
+ * @param str
+ * @returns {boolean}
+ */
+export function isRTSP(str) {
+  const reg = /^rtsp:\/\/([a-z]{0,10}:.{0,10}@)?(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+  const reg1 = /^rtsp:\/\/([a-z]{0,10}:.{0,10}@)?(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]):[0-9]{1,5}/
+  const reg2 = /^rtsp:\/\/([a-z]{0,10}:.{0,10}@)?(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\//
+  return reg.test(str) || reg1.test(str) || reg2.test(str)
 }
